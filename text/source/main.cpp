b28603b9c9c6379573edc95366b260d83d0c89d9
zerasul <zerasul@gmail.com>
2022-02-05 12:38:38 +0100

    adding text example

---
+++ text/source/main.cpp
@@ -0,0 +1,35 @@
+#include <nds.h>
+#include <stdio.h>
+
+int main(){
+    PrintConsole topScreen;
+	PrintConsole bottomScreen;
+
+    //Init main and sub consoles to text mode
+    videoSetMode(MODE_0_2D);
+	videoSetModeSub(MODE_0_2D);
+
+	vramSetBankA(VRAM_A_MAIN_BG);
+	vramSetBankC(VRAM_C_SUB_BG);
+
+    consoleInit(&topScreen, 3,BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+	consoleInit(&bottomScreen, 3,BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
+
+    consoleSelect(&topScreen);
+	iprintf("\n\n\tLa Jaqueria\n");
+	iprintf("\tTest texto Superior\n");
+	iprintf("\twww.lajaqueria.org");
+
+	consoleSelect(&bottomScreen);   
+
+    iprintf("\n\n\tLa Jaqueria\n");
+	iprintf("\tTest texto Inferior\n");
+	iprintf("\twww.lajaqueria.org");
+
+    while(true){
+        swiWaitForVBlank();
+
+    }
+
+    return 0;
+}
