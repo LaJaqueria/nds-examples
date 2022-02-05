356db374fe8b073d27fe3a7afc8a529a0d7e6b13
zerasul <zerasul@gmail.com>
2022-02-05 13:46:48 +0100

    añadiendo ejemplo con 2 fondos

---
+++ background2/source/main.cpp
@@ -0,0 +1,35 @@
+
+#include <nds.h>
+#include <stdio.h>
+
+#include "background.h"
+#include "fondo2.h"
+
+int main(void){
+    videoSetMode(MODE_5_2D);
+    videoSetModeSub(MODE_5_2D);
+   	consoleDemoInit();
+
+
+    vramSetBankA(VRAM_A_MAIN_BG);
+
+	
+
+	// set up our bitmap background
+	bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
+	
+	decompress(backgroundBitmap, BG_GFX,  LZ77Vram);
+
+    int bgsub=bgInitSub(3, BgType_Bmp8, BgSize_B8_256x256, 5,0); 
+
+    dmaCopy(fondo2Bitmap,bgGetGfxPtr(bgsub),256*217);
+    dmaCopy(fondo2Pal,BG_PALETTE_SUB,256*2);
+    
+    while(true){
+        swiWaitForVBlank();
+		scanKeys();
+		if (keysDown()&KEY_START) break;
+    }
+
+    return 0;
+}
