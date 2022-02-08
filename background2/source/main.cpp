8d9539281613eac1765cf6f35a74773bebe0092b
zerasul <zerasul@gmail.com>
2022-02-08 19:11:21 +0100

    formato

--- background2/source/main.cpp
+++ background2/source/main.cpp
@@ -5,30 +5,30 @@
 #include "background.h"
 #include "fondo2.h"
 
-int main(void){
+int main(void)
+{
     videoSetMode(MODE_5_2D);
     videoSetModeSub(MODE_5_2D);
-   	consoleDemoInit();
-
+    consoleDemoInit();
 
     vramSetBankA(VRAM_A_MAIN_BG);
 
-	
+    // set up our bitmap background
+    bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
 
-	// set up our bitmap background
-	bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
-	
-	decompress(backgroundBitmap, BG_GFX,  LZ77Vram);
+    decompress(backgroundBitmap, BG_GFX, LZ77Vram);
 
-    int bgsub=bgInitSub(2, BgType_Bmp8, BgSize_B8_256x256, 5,0); 
+    int bgsub = bgInitSub(2, BgType_Bmp8, BgSize_B8_256x256, 5, 0);
 
-    dmaCopy(fondo2Bitmap,bgGetGfxPtr(bgsub),256*256);
-    dmaCopy(fondo2Pal,BG_PALETTE_SUB,256*2);
-    
-    while(true){
+    dmaCopy(fondo2Bitmap, bgGetGfxPtr(bgsub), 256 * 256);
+    dmaCopy(fondo2Pal, BG_PALETTE_SUB, 256 * 2);
+
+    while (true)
+    {
         swiWaitForVBlank();
-		scanKeys();
-		if (keysDown()&KEY_START) break;
+        scanKeys();
+        if (keysDown() & KEY_START)
+            break;
     }
 
     return 0;
--- text/source/main.cpp
+++ text/source/main.cpp
@@ -1,35 +1,36 @@
 #include <nds.h>
 #include <stdio.h>
 
-int main(){
-    PrintConsole topScreen;
+int main()
+{
+	PrintConsole topScreen;
 	PrintConsole bottomScreen;
 
-    //Init main and sub consoles to text mode
-    videoSetMode(MODE_0_2D);
+	//Init main and sub consoles to text mode
+	videoSetMode(MODE_0_2D);
 	videoSetModeSub(MODE_0_2D);
 
 	vramSetBankA(VRAM_A_MAIN_BG);
 	vramSetBankC(VRAM_C_SUB_BG);
 
-    consoleInit(&topScreen, 3,BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
-	consoleInit(&bottomScreen, 3,BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
+	consoleInit(&topScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+	consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
 
-    consoleSelect(&topScreen);
+	consoleSelect(&topScreen);
 	iprintf("\n\n\tLa Jaqueria\n");
 	iprintf("\tTest texto Superior\n");
 	iprintf("\twww.lajaqueria.org\x1b[39m");
 
-	consoleSelect(&bottomScreen);   
+	consoleSelect(&bottomScreen);
 
-    iprintf("\n\n\tLa Jaqueria\n");
+	iprintf("\n\n\tLa Jaqueria\n");
 	iprintf("\tTest texto Inferior\n");
 	iprintf("\t\twww.lajaqueria.org");
 
-    while(true){
-        swiWaitForVBlank();
+	while (true)
+	{
+		swiWaitForVBlank();
+	}
 
-    }
-
-    return 0;
+	return 0;
 }
