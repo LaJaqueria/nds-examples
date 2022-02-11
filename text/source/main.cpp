1466221136f3622caa2916408088dc3c04f79d75
zerasul <zerasul@gmail.com>
2022-02-11 19:23:00 +0100

    updating text example

--- text/source/main.cpp
+++ text/source/main.cpp
@@ -9,7 +9,7 @@ int main()
 	//Init main and sub consoles to text mode
 	videoSetMode(MODE_0_2D);
 	videoSetModeSub(MODE_0_2D);
-
+	consoleDemoInit();
 	vramSetBankA(VRAM_A_MAIN_BG);
 	vramSetBankC(VRAM_C_SUB_BG);
 
@@ -17,15 +17,15 @@ int main()
 	consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
 
 	consoleSelect(&topScreen);
-	iprintf("\n\n\tLa Jaqueria\n");
-	iprintf("\tTest texto Superior\n");
-	iprintf("\twww.lajaqueria.org\x1b[39m");
+	iprintf("\n\n\t\tLa Jaqueria\n");
+	iprintf("\t\x1b[31mTest texto Superior\n");
+	iprintf("\t\x1b[35mwww.lajaqueria.org");
 
 	consoleSelect(&bottomScreen);
 
-	iprintf("\n\n\tLa Jaqueria\n");
-	iprintf("\tTest texto Inferior\n");
-	iprintf("\t\twww.lajaqueria.org");
+	iprintf("\x1b[1;5HLa Jaqueria\n");
+	iprintf("\x1b[2;2H\x1b[31mTest texto Inferior\n");
+	iprintf("\x1b[3;2H\x1b[32mwww.lajaqueria.org");
 
 	while (true)
 	{
